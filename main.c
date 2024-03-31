92b3f9cff551df9f6f94ba28e5f5b19283cd5152
Isabelle Beaudry Hajji <100730620+isabelle-bh@users.noreply.github.com>
2024-03-30 21:54:53 -0400

    starting switch plant profile implementation

--- main.c
+++ main.c
@@ -2,25 +2,29 @@
 #include "adc_functions.h"
 #include <stdio.h>
 #include <stdint.h>
+
 /*
 #define LED_BASE 0xFF200000
 #define HEX3_HEX0_BASE 0xFF200020
 #define HEX3_HEX1_BASE 0xFF200030
 #define SW_BASE 0xFF200040
-#define BTN_BASE 0xFF200050
 #define TIMER_BASE 0xFFFEC600
 #define ADC_C0 0xFF204000
 #define ADC_C1 0xFF204004       // write 1 to bit 15 for auto-update
 #define mask_12_bits 0x00000FFF // 12 lsb to read channel input
 #define LED_BASE 0xFF200060     // write led voltage to this
 #define LED_DIR 0xFF200064      // write 1 to specify output
-#define SW_BASE 0xFF200040      // to select which volatge
 */
 
+#define BTN_BASE 0xFF200050
 #define SW_BASE 0x00000000
 
-volatile int timer_done = 0; // 1 when its been x amount of time
+extern struct species_profile plant_profiles[];
+#define NUM_SWITCHES 4
+
 volatile struct species_profile current_plant;
+
+volatile int timer_done = 0; // 1 when its been x amount of time
 
 int checkTimer(void)
 {
@@ -38,20 +42,18 @@ int getButtonInputs(void)
 {
 }
 
-void setCurrentPlant(void)
-{
-    /*
+// this function reads the value from the switch memory-mapped register
+void setCurrentPlant(void) {
     volatile unsigned int *switchPtr = (unsigned int *)SW_BASE;
-    int switchState = *switchPtr;
-    if (switchState < sizeof(*plant_profiles))
-    {
-        // current_plant = plant_profiles[switchState];
-        current_plant = plant_profiles[2];
-        printf("%s\n", current_plant.name);
-    }*/
+    unsigned int switchState = *switchPtr;
 
-    current_plant = plant_profiles[2];
-    printf("%s\n", current_plant.name);
+    // Ensure that the switchState is within the range of available profiles
+    if (switchState < NUM_SWITCHES) {
+        current_plant = plant_profiles[switchState];
+        printf("Current plant set to: %s\n", current_plant.name);
+    } else {
+        printf("Invalid switch state: %u\n", switchState);
+    }
 }
 
 void displayHex()
@@ -62,10 +64,10 @@ int main(void)
 {
     while (1)
     {
-        struct plant_status p1 = {2.0, 3.0};
-        // int moisture = get_moisture(1);
         setCurrentPlant();
+        int moisture = get_moisture(1);
 
-        return 0;
     }
+
+    return 0;
 }
